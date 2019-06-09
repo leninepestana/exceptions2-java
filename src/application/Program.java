322a7908353b3a36392605a84a2fb52925fff28d
Lenine Pestana <leninepestana@gmail.com>
2019-06-09 20:51:15 +0100

    Not so good solution

---
+++ src/application/Program.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainExceptions;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int accountNumber = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.next();
+			String accountHolder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double accountBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			System.out.println("Initialized constructor");
+			Account account = new Account(accountNumber, accountHolder, accountBalance, withdrawLimit);
+			
+			System.out.println();
+			
+			System.out.print("Enter amount for withdraw: ");
+			double withdrawAmount = sc.nextDouble();
+			
+			account.withdraw(withdrawAmount);
+			
+			System.out.println(account);
+			
+		}
+		catch (DomainExceptions e) {
+			System.out.println(e.getMessage());
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Error not a number ");
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ src/model/entities/Account.java
@@ -0,0 +1,77 @@
+package model.entities;
+
+import model.exceptions.DomainExceptions;
+
+public class Account {
+	private Integer accountNumber;
+	private String accountHolder;
+	private Double accountBalance;
+	private Double withdrawLimit;
+	
+	public Account() {
+	}
+
+	public Account(Integer accountNumber, String accountHolder, Double accountBalance, Double withdrawLimit) {
+		this.accountNumber = accountNumber;
+		this.accountHolder = accountHolder;
+		this.accountBalance = accountBalance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(Integer accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getAccountHolder() {
+		return accountHolder;
+	}
+
+	public void setAccountHolder(String accountHolder) {
+		this.accountHolder = accountHolder;
+	}
+
+	public Double getAccountBalance() {
+		return accountBalance;
+	}
+	
+	/*
+	public void setAccountBalance(Double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	*/
+	
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	/*
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	*/
+	
+	public void deposit(double depositAmount) {
+		accountBalance += depositAmount;
+	}
+	
+	public void withdraw(double withdrawAmount) throws DomainExceptions {
+		if (withdrawAmount > withdrawLimit ) {
+			throw new DomainExceptions("Withdraw error: The amount exceeds withdraw limit");
+		}
+		if (withdrawAmount > accountBalance) {
+			throw new DomainExceptions("Withdraw error: Not enough balance");
+		}
+		accountBalance -= withdrawAmount;
+	}
+	
+	@Override
+	public String toString() {
+		return "New balance: "
+				+ accountBalance;
+	}
+	
+}
